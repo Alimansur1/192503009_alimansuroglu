00b975fad9ae0874f4aa5bb690d637915de1ea85
Ali MANSUROĞLU <alimansur_21@icloud.com>
2024-03-19 10:47:06 +0300

    Update conf.php

--- database/conf.php
+++ database/conf.php
@@ -5,9 +5,36 @@ $password = "";
 $database = "donemproje";
 
 try {
-    $pdo = new PDO("mysql:host=$servername;dbname=$database;charset=utf8", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Veritabanına bağlantı hatası: " . $e->getMessage());
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Güvenlik için SQL Injection önlemi
+    $username = htmlspecialchars($username);
+    $password = htmlspecialchars($password);
+
+    // Kullanıcı adı ve şifreyi veritabanında kontrol et
+    $stmt = $conn->prepare("SELECT * FROM kullanici WHERE username=:username AND password=:password");
+    $stmt->bindParam(':username', $username);
+    $stmt->bindParam(':password', $password);
+    $stmt->execute();
+
+
+    if ($stmt->rowCount() > 0) {
+
+        echo "Giriş başarılı. Hoş geldiniz, $username!";
+    } else {
+
+        echo "Giriş başarısız. Kullanıcı adı veya şifre hatalı!";
+    }
 }
+catch(PDOException $e) {
+
+    echo "Bağlantı hatası: " . $e->getMessage();
+}
+
+
+$conn = null;
 ?>
